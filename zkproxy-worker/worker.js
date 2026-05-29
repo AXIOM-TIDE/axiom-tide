ad8b8aabb99afad763d186fa12e02e482c09f947
Franklin <franklin@axiomtide.com>
2026-05-28 21:43:14 -0500

    feat: Walrus publisher URL configurable via WALRUS_PUBLISHER_URL env var

--- zkproxy-worker/worker.js
+++ zkproxy-worker/worker.js
@@ -312,7 +312,7 @@ export default {
 
         const epochs = url.searchParams.get('epochs') || '5'
         const resp   = await fetch(
-          'https://publisher.walrus-mainnet.walrus.space/v1/blobs?epochs=' + epochs,
+          (env.WALRUS_PUBLISHER_URL || 'https://publisher.walrus-mainnet.walrus.space') + '/v1/blobs?epochs=' + epochs,
           { method: 'PUT', headers: { 'Content-Type': ct }, body: bytes },
         )
         const text = await resp.text()
